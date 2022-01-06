b1ba3ac776c629ee1f4ef1bea8e8164f740fc51b
Sheldon Kwok <sheldon@secondspectrum.com>
2022-01-06 10:28:12 -0800

    Add nvim.lua config

---
+++ nvim/init.lua
@@ -0,0 +1,80 @@
+require('packer').startup(function()
+  -- Packer 
+  use 'wbthomason/packer.nvim'
+  use 'nvim-lua/plenary.nvim'
+
+  -- Movement
+  use 'scrooloose/nerdtree'
+  use 'ibhagwan/fzf-lua'
+
+  -- Completion
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+
+  -- TypeScript
+  use "neovim/nvim-lspconfig"
+  use "jose-elias-alvarez/null-ls.nvim"
+  use "jose-elias-alvarez/nvim-lsp-ts-utils"
+
+  -- HashiCorp
+  use 'hashivim/vim-terraform'
+
+  -- Other file types
+  use 'blankname/vim-fish'
+end)
+
+require("lsp-config")
+
+vim.cmd [[
+  colorscheme fmj
+  set background=dark
+
+  set noswapfile
+
+	filetype plugin indent on
+	set ai
+	set expandtab
+	set tabstop=2
+	set shiftwidth=2
+	set autoindent
+	set smarttab
+	retab
+
+	" remaps
+	let mapleader = ","
+  noremap <Leader><Leader> :q <CR>
+	nnoremap ; :
+	nnoremap : ;
+
+  set hlsearch
+  set incsearch
+  set ignorecase
+  set smartcase
+
+  set nowrap
+  set incsearch
+
+  set ttyfast
+  set lazyredraw
+  set ff=unix
+
+  " tabs
+  nnoremap tt :tabnew .<CR>
+  nnoremap tn1 :tabn 1<CR>
+  nnoremap tn2 :tabn 2<CR>
+  nnoremap tn3 :tabn 3<CR>
+  nnoremap tn4 :tabn 4<CR>
+
+  " autocmd BufEnter * execute "chdir ".escape(expand("%:p:h"), ' ')
+  set autochdir
+
+  autocmd BufWritePre *.tf lua vim.lsp.buf.formatting_sync()
+]]
+
+vim.api.nvim_set_keymap('n', '<Leader>f',
+    "<cmd>lua require('fzf-lua').files()<CR>",
+    { noremap = true, silent = true })
+
---
+++ nvim/lua/lsp-config.lua
@@ -0,0 +1,138 @@
+local lspconfig = require('lspconfig')
+local null_ls = require('null-ls')
+local cmp = require('cmp')
+
+-- cmp
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
+  },
+  mapping = {
+    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ['<C-e>'] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+-- Setup lspconfig.
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+-- LSP
+local buf_map = function(bufnr, mode, lhs, rhs, opts)
+    vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts or {
+        silent = true,
+    })
+end
+
+local on_attach = function(client, bufnr)
+    vim.cmd('command! LspDef lua vim.lsp.buf.definition()')
+    vim.cmd('command! LspFormatting lua vim.lsp.buf.formatting()')
+    vim.cmd('command! LspCodeAction lua vim.lsp.buf.code_action()')
+    vim.cmd('command! LspHover lua vim.lsp.buf.hover()')
+    vim.cmd('command! LspRename lua vim.lsp.buf.rename()')
+    vim.cmd('command! LspRefs lua vim.lsp.buf.references()')
+    vim.cmd('command! LspTypeDef lua vim.lsp.buf.type_definition()')
+    vim.cmd('command! LspImplementation lua vim.lspbuf.implementation()')
+    vim.cmd('command! LspDiagPrev lua vim.diagnostic.goto_prev()')
+    vim.cmd('command! LspDiagNext lua vim.diagnostic.goto_next()')
+    vim.cmd('command! LspDiagLine lua vim.diagnostic.open_float()')
+    vim.cmd('command! LspSignatureHelp lua vim.lsp.buf.signature_help()')
+
+    buf_map(bufnr, 'n', 'gd', ':LspDef<CR>')
+    buf_map(bufnr, 'n', 'gr', ':LspRename<CR>')
+    buf_map(bufnr, 'n', 'gy', ':LspTypeDef<CR>')
+    buf_map(bufnr, 'n', 'K', ':LspHover<CR>')
+    buf_map(bufnr, 'n', '[a', ':LspDiagPrev<CR>')
+    buf_map(bufnr, 'n', ']a', ':LspDiagNext<CR>')
+    buf_map(bufnr, 'n', 'ga', ':LspCodeAction<CR>')
+    buf_map(bufnr, 'n', '<Leader>a', ':LspDiagLine<CR>')
+    buf_map(bufnr, 'i', '<C-x><C-x>', '<cmd> LspSignatureHelp<CR>')
+
+    if client.resolved_capabilities.document_formatting then
+        vim.cmd('autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()')
+    end
+end
+
+-- TypeScript
+lspconfig.tsserver.setup({
+    on_attach = function(client, bufnr)
+        client.resolved_capabilities.document_formatting = false
+        client.resolved_capabilities.document_range_formatting = false
+
+        local ts_utils = require('nvim-lsp-ts-utils')
+        ts_utils.setup({})
+        ts_utils.setup_client(client)
+
+        buf_map(bufnr, 'n', 'gs', ':TSLspOrganize<CR>')
+        buf_map(bufnr, 'n', 'gi', ':TSLspRenameFile<CR>')
+        buf_map(bufnr, 'n', 'go', ':TSLspImportAll<CR>')
+
+        on_attach(client, bufnr)
+    end,
+
+    capabilties = capabilities
+})
+
+-- Terraform
+lspconfig.terraformls.setup{} 
+
+-- YAML
+lspconfig.yamlls.setup{
+  settings = {
+    yaml = {
+      schemas = {
+        ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "/*.k8s.yaml"
+        -- ["file:///Users/sheldon/.config/nvim/schemas/k8s-v1.18.0-standalone-strict.json"] = "/*.k8s.yaml"
+      }
+    }
+  }
+}
+
+-- null ls
+null_ls.setup({
+    sources = {
+        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.code_actions.eslint,
+        null_ls.builtins.formatting.prettier,
+        -- null_ls.builtins.formatting.terraform_fmt,
+    },
+    on_attach = on_attach,
+})
+
